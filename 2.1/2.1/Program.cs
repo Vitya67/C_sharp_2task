1011809855252f0469616358bb89d9fc8d758248
Vitya67 <159480815+Vitya67@users.noreply.github.com>
2024-10-06 23:39:23 +0300

    Add files via upload

---
+++ 2.1/2.1/Program.cs
@@ -0,0 +1,17 @@
+﻿        Console.Write("Введіть число: ");
+
+        double number = Convert.ToDouble(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine("Число додатнє.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("Число від'ємне.");
+        }
+        else
+        {
+            Console.WriteLine("Число дорівнює нулю.");
+        }
+   
