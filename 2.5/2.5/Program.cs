922a6b50cc20e888c3014eaf62ff600660cbc1dd
Vitya67 <159480815+Vitya67@users.noreply.github.com>
2024-10-06 23:46:50 +0300

    Add files via upload

---
+++ 2.5/2.5/Program.cs
@@ -0,0 +1,22 @@
+﻿        Console.Write("Введіть перше число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть третє число: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        double max = num1;
+
+        if (num2 > max)
+        {
+            max = num2;
+        }
+
+        if (num3 > max)
+        {
+            max = num3;
+        }
+        Console.WriteLine($"Найбільше число: {max}");
+    
