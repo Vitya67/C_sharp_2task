4db7a37b819c6d8a80611436e9c52d9d89664b1e
Vitya67 <159480815+Vitya67@users.noreply.github.com>
2024-10-06 23:54:29 +0300

    Add files via upload

---
+++ 2.7/2.7/Program.cs
@@ -0,0 +1,19 @@
+﻿ Console.Write("Введіть число: ");
+ int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число є парним.");
+}
+else
+{
+    Console.WriteLine("Число є непарним.");
+}
+if (number % 3 == 0)
+{
+    Console.WriteLine("Число ділиться на 3.");
+}
+else
+{
+    Console.WriteLine("Число не ділиться на 3.");
+}
