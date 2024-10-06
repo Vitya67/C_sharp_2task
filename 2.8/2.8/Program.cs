700192d2a2970dd004b7783a5a12855d0925a239
Vitya67 <159480815+Vitya67@users.noreply.github.com>
2024-10-06 23:57:33 +0300

    Add files via upload

---
+++ 2.8/2.8/Program.cs
@@ -0,0 +1,25 @@
+﻿        Console.Write("Введіть довжину сторони a: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть довжину сторони b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введіть довжину сторони c: ");
+        double c = Convert.ToDouble(Console.ReadLine());
+
+        double longest = a;
+        string longestSide = "a";
+
+        if (b > longest)
+        {
+            longest = b;
+            longestSide = "b";
+        }
+
+        if (c > longest)
+        {
+            longest = c;
+            longestSide = "c";
+        }
+
+        Console.WriteLine($"Найдовша сторона: {longestSide}, її довжина: {longest}");
