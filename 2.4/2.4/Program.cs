ae429f732be86df23afef366a3167480bc2a2666
Vitya67 <159480815+Vitya67@users.noreply.github.com>
2024-10-06 23:43:52 +0300

    Add files via upload

---
+++ 2.4/2.4/Program.cs
@@ -0,0 +1,16 @@
+﻿        Console.Write("Введіть символ: ");
+
+        char symbol = Convert.ToChar(Console.ReadLine().ToLower());
+
+        if (symbol == 'a' || symbol == 'e' || symbol == 'i' || symbol == 'o' || symbol == 'u')
+        {
+            Console.WriteLine("Цей символ є голосною.");
+        }
+              else if (char.IsLetter(symbol))
+        {
+            Console.WriteLine("Цей символ є приголосною.");
+        }
+        else
+        {
+            Console.WriteLine("Цей символ не є літерою.");
+        }
