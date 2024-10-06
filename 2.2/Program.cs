4fb512016919aa1d8aa40768a126cb9fc5167614
Vitya67 <kapitansd35@gmail.com>
2024-10-06 23:27:31 +0300

    Add project files.

---
+++ 2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿        Console.Write("Введіть рік: ");
+
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        {
+            Console.WriteLine($"{year} рік є високосним.");
+        }
+        else
+        {
+            Console.WriteLine($"{year} рік не є високосним.");
+        }
+   
