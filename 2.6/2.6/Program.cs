5929f1d0f2d9df9506ed21339186e8d58737562b
Vitya67 <159480815+Vitya67@users.noreply.github.com>
2024-10-06 23:50:53 +0300

    Add files via upload

---
+++ 2.6/2.6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введіть кількість років: ");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age >= 12 && age <= 18)
+{
+    Console.WriteLine("Цей вік є підлітковим.");
+}
+else
+{
+    Console.WriteLine("Цей вік не є підлітковим.");
+}
